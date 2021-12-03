1c75a37574deb91b79c93030113b04d9759cf167
tuandat <huynhtuandat05december@gmail.com>
2021-12-04 00:03:18 +0700

    Test lint-staged

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPageWithLayout = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js</a>
         </h1>
 
         <p className={styles.description}>
