038bd4ca35e8a5e15355e78ae104c4fe43527588
tuandat <huynhtuandat05december@gmail.com>
2021-12-05 03:30:17 +0700

    auth api

--- pages/api/[...param].ts
+++ pages/api/[...param].ts
@@ -1,3 +1,4 @@
+import Cookies from 'cookies';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import httpProxy from 'http-proxy';
@@ -12,10 +13,20 @@ export const config = {
 };
 const proxy = httpProxy.createProxyServer();
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  req.headers.cookie = '';
-  proxy.web(req, res, {
-    target: process.env.API_URL,
-    changeOrigin: true,
-    selfHandleResponse: false,
+  return new Promise((resovle, reject) => {
+    const cookies = new Cookies(req, res);
+    const accessToken = cookies.get('access_token');
+    req.headers.cookie = '';
+    if (accessToken) {
+      req.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    });
+    proxy.once('proxyRes', () => {
+      resovle(true);
+    });
   });
 }
